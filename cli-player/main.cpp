2492fde21fc233d9fb3e658b7360b8a756b70758
tao-lj <ljtao7871@gmail.com>
2025-01-10 13:33:31 +0800

    对代码进行重构并添加了前进/后退功能

--- cli-player/main.cpp
+++ cli-player/main.cpp
@@ -1,284 +1,329 @@
 #include <opencv2/opencv.hpp>
-#include <SDL.h>
-#include <windows.h>
+#include <SDL_mixer.h>
+#include <Windows.h>
 #include <conio.h>
-#include <fstream>
+#include <iostream>
 
-#define SPACE	32
-#define ESC	27
+const int PAUSE_KEY = 32;
+const int ESC_KEY = 27;
+const int FORWARD_KEY = 77;
+const int BACKWARD_KEY = 75;
 
-#undef main
-#undef max
+double eps = 2.7;
 
-using namespace cv;
-using namespace std;
-using namespace chrono;
+double skipInterval = 5.0;
 
-double exponential = 2.7;		// 控制丢帧时阈值的指数常量
-string ffmpegPath = "ffmpeg";		// ffmpeg路径
-string tmpAudioFile = "TmpAudio.wav";	// 临时音频文件名称
+std::string ffmpegPath = "ffmpeg";
+std::string audioName = "tmp.mp3";
 
-VideoCapture video;
-
-SDL_AudioDeviceID deviceId;	// 音频设备 ID
-SDL_AudioSpec wavSpec;		// 音频规格
-Uint8* wavBuffer;		// 音频缓冲区
-Uint32 wavLength;		// 音频长度
+Mix_Music* music;
 
 bool isPaused = false;
-double currentTime, pausedTime;
+double currentTime;
+double pauseTime;
+std::chrono::system_clock::time_point startTime;
 
+cv::VideoCapture video;
 
-// 获取设置文件
-void get_cfg() {
-	ifstream cfg("config.txt");
-	string str;
+int videoFPS;
+int videoWidth;
+int videoHeight;
 
-	// 若设置文件不存在则创建
-	if (!cfg.is_open()) {
-		cout << "Warning: 无法打开配置文件config.txt" << endl;
-		ofstream fp("config.txt");
-		fp << "ffmpegPath=" << ffmpegPath << endl;
-		fp << "exponential=" << exponential;
-		return;
-	}
-
-	// 逐行获取设置选项
-	while (getline(cfg, str)) {
-		auto a = str.find("=");
-		if (a == string::npos) continue;
-		string key = str.substr(0, a);
-		string value = str.substr(a + 1, str.size() - 1);
-
-		if (key == "ffmpegPath") ffmpegPath = value;
-		else if (key == "exponential") exponential = stod(value);
-	}
-}
-
-
-// 初始化控制台与SDL2
-void init() {
+// 初始化视频/音频文件
+int init_resources(std::string videoName) {
 	// 启用控制台虚拟终端序列
 	DWORD dwMode = 0;
 	GetConsoleMode(GetStdHandle(STD_OUTPUT_HANDLE), &dwMode);
 	SetConsoleMode(GetStdHandle(STD_OUTPUT_HANDLE), dwMode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
 
-	// 初始化 SDL2 用于播放音频
-	if (SDL_Init(SDL_INIT_AUDIO) < 0) {
-		cerr << "ERROR: SDL 初始化失败: " << SDL_GetError() << endl;
-		exit(1);
+	// 打开视频文件
+	if (!video.open(videoName)) {
+		std::cerr << "ERROR: 无法打开视频文件" << std::endl;
+		return -1;
+	}
+
+	// 提取音频文件
+	std::string command = ffmpegPath + " -i \"" + videoName + "\" -f mp3 -vn " + audioName;
+	if (system(command.c_str()) != 0) { // 检查 ffmpeg 命令是否执行成功
+		std::cerr << "ERROR: 无法提取音频文件" << std::endl;
+		video.release();
+		return -1;
+	}
+
+	// 初始化SDL_mixer
+	if (Mix_Init(MIX_INIT_MP3) < 0) {
+		std::cerr << "ERROR：无法初始化SDL_mixer:" << Mix_GetError() << std::endl;
+		video.release();
+		return -1;
+	}
+
+	// 开启音频设备
+	if (Mix_OpenAudio(22050, MIX_DEFAULT_FORMAT, 2, 4096) < 0) {
+		std::cerr << "ERROR: 无法开启音频设备:" << Mix_GetError() << std::endl;
+		video.release();
+		Mix_Quit();
+		return -1;
+	}
+
+	//加载音频文件
+	music = Mix_LoadMUS(audioName.c_str());
+	if (music == NULL) {
+		std::cerr << "ERROR: 无法加载音频文件:" << Mix_GetError() << std::endl;
+		video.release();
+		Mix_CloseAudio();
+		Mix_Quit();
+		return -1;
+	}
+
+	// 保存视频信息
+	videoFPS = video.get(cv::CAP_PROP_FPS);
+	videoWidth = video.get(cv::CAP_PROP_FRAME_WIDTH);
+	videoHeight = video.get(cv::CAP_PROP_FRAME_HEIGHT);
+
+	return 0;
+}
+
+// 清理资源并退出
+void quit(int exitValue) {
+	video.release();
+	Mix_HaltMusic();
+	Mix_FreeMusic(music);
+	Mix_CloseAudio();
+	Mix_Quit();
+	remove(audioName.c_str());
+	puts("\33[0m\n\33[?25h"); // 恢复控制台设置
+	exit(exitValue);
+}
+
+// 计算字符画尺寸
+void get_size(int& width, int& height) {
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+	width = (csbi.srWindow.Right - csbi.srWindow.Left) << 1;
+	height = (csbi.srWindow.Bottom - csbi.srWindow.Top) << 1;
+
+	double aspectRatio = (double)csbi.dwSize.X / width / csbi.dwSize.Y * height;
+
+	// 调整宽高比
+	if ((double)videoWidth / width > aspectRatio * videoHeight / height) {
+		height = aspectRatio * width / videoWidth * videoHeight;
+		height = MIN(height, videoHeight);
+		height &= ~1; // 确保高度为偶数
+	}
+	else {
+		width = (double)height / videoHeight * videoWidth / aspectRatio;
+		width = MIN(width, videoWidth);
 	}
 }
 
+// 计算颜色差异是否超过阈值
+bool cmp(cv::Vec3b x, cv::Vec3b y, int threshold) {
+	int dr = x[2] - y[2];
+	int dg = x[1] - y[1];
+	int db = x[0] - y[0];
+	int distSq = dr * dr + dg * dg + db * db;
+	return distSq > threshold * threshold;
+}
 
-// 获取视频/音频文件
-void get_file(int argc, char* argv[]) {
-	// 获取视频文件路径
-	string videoName = "";
-	if (argc < 2) {
-		cout << "请输入视频路径：";
-		getline(cin, videoName);
+// 打印当前帧
+int render_frame(cv::Mat& lastFrame, unsigned long long& lastIdx) {
+	// 获取当前时间所对应的帧的编号
+	unsigned long long idx = videoFPS * currentTime;
+
+	if (idx < 0 || idx >= video.get(cv::CAP_PROP_FRAME_COUNT)) return 1;
+	if (lastIdx == idx && lastIdx && !isPaused) return 0;
+
+	cv::Mat frame;
+	cv::Vec3b bgColor = { 0, 0, 0 };	// 背景颜色
+	cv::Vec3b txtColor = { 0, 0, 0 };	// 文本颜色
+	std::string output = "";
+	bool isOpened = true;
+	bool firstFrame = true;
+	bool colorChanged = true;
+	long long lost = idx - lastIdx;
+	int threshold = MIN(pow(lost, eps), 128); // 根据丢帧数调整颜色差异判断阈值
+	int width, height;
+	get_size(width, height);
+
+	// 获取当前帧
+	if (lost > 0) {
+		for (int i = 0; i < lost; ++i) {
+			if (!video.grab()) {
+				puts("\33[0m\n\33[?25h"); // 恢复控制台设置
+				std::cerr << "ERROR: 读取视频时出错" << std::endl;
+				quit(-1);
+			}
+		}
+		isOpened = video.retrieve(frame);
 	}
-	else videoName += argv[1];
+	else {
+		video.set(cv::CAP_PROP_POS_FRAMES, idx);
+		isOpened = video.read(frame);
+	}
+
+	if (!isOpened) {
+		puts("\33[0m\n\33[?25h");
+		std::cerr << "ERROR: 解码视频时出错" << std::endl;
+		quit(-1);
+	}
+
+	cv::resize(frame, frame, { width, height });
+
+	// 如果尺寸改变则清屏
+	if (lastFrame.cols != width || lastFrame.rows != height) {
+		lastFrame = cv::Mat(width, height, CV_8UC1, cv::Scalar(0, 0, 0));
+		puts("\33c");
+	}
+
+	// 生成字符画
+	for (int i = 0; i < height; i += 2) {
+		for (int j = 0; j < width; ++j) {
+			cv::Vec3b nbgColor = frame.at<cv::Vec3b>(i, j);
+			cv::Vec3b ntxtColor = frame.at<cv::Vec3b>(i + 1, j);
+
+			if (!cmp(nbgColor, lastFrame.at<cv::Vec3b>(i, j), threshold) &&
+				!cmp(ntxtColor, lastFrame.at<cv::Vec3b>(i + 1, j), threshold)) { // 颜色未改变则跳过
+				frame.at<cv::Vec3b>(i, j) = lastFrame.at<cv::Vec3b>(i, j);
+				frame.at<cv::Vec3b>(i + 1, j) = lastFrame.at<cv::Vec3b>(i + 1, j);
+				firstFrame = true;
+				continue;
+			}
+
+			if (firstFrame) {
+				firstFrame = colorChanged = false;
+				output += "\33[" + std::to_string(i / 2 + 1) + ';' + std::to_string(j + 1) + 'H'; // 移动光标
+			}
+
+			if (cmp(nbgColor, bgColor, threshold)) { // 改变背景颜色
+				output += "\33[48;2;" + std::to_string(nbgColor[2]) + ';'
+					+ std::to_string(nbgColor[1]) + ';'
+					+ std::to_string(nbgColor[0]) + 'm';
+				bgColor = nbgColor;
+			}
+			else {
+				frame.at<cv::Vec3b>(i, j) = bgColor;
+			}
+
+			if (cmp(ntxtColor, txtColor, threshold)) { // 改变文本颜色
+				output += "\33[38;2;" + std::to_string(ntxtColor[2]) + ';'
+					+ std::to_string(ntxtColor[1]) + ';'
+					+ std::to_string(ntxtColor[0]) + 'm';
+				txtColor = ntxtColor;
+			}
+			else {
+				frame.at<cv::Vec3b>(i + 1, j) = txtColor;
+			}
+			output += "▄";
+		}
+		if (!firstFrame && i + 2 < height) output += '\n';
+	}
+
+	// 打印字符画
+	if (!colorChanged) puts(output.c_str());
+
+	lastFrame = frame;
+	lastIdx = idx;
+	return 0;
+}
+
+// 键盘事件处理
+void handle_keyboard() {
+	if (!_kbhit()) return;
+
+	int ch = _getch();
+
+	switch (ch) {
+	case PAUSE_KEY:		// 暂停或恢复播放
+		isPaused = !isPaused;
+		if (isPaused) Mix_PauseMusic();
+		else Mix_ResumeMusic();
+		break;
+	case ESC_KEY:		// 退出播放
+		quit(0);
+		break;
+	case FORWARD_KEY:	// 快进视频
+		currentTime += skipInterval;
+		if (currentTime < Mix_MusicDuration(music)) {
+			Mix_SetMusicPosition(currentTime);
+		}
+		else {
+			quit(0);
+		}
+		if (!isPaused) {
+			pauseTime = std::chrono::duration<double>(std::chrono::system_clock::now() - startTime).count() - currentTime;
+		}
+		break;
+	case BACKWARD_KEY:	// 快退视频
+		currentTime -= skipInterval;
+		if (currentTime > 0) {
+			Mix_SetMusicPosition(currentTime);
+		}
+		else {
+			currentTime = 0;
+			Mix_SetMusicPosition(currentTime);
+		}
+		if (!isPaused) {
+			pauseTime = std::chrono::duration<double>(std::chrono::system_clock::now() - startTime).count() - currentTime;
+		}
+		break;
+	default:
+		break;
+	}
+}
+
+// 视频播放主函数
+void play() {
+	cv::Mat lastFrame = cv::Mat(1, 1, CV_8UC1, cv::Scalar(0, 0, 0));
+	unsigned long long lastIdx = 0;
+
+	// 清屏并隐藏光标
+	puts("\33c\n\33[?25l\n\33[30;40m");
+
+	// 播放音乐
+	if (Mix_PlayMusic(music, -1) == -1) {
+		puts("\33[0m\n\33[?25h");
+		printf("ERROR: 无法播放音频 %s\n", Mix_GetError());
+		quit(-1);
+	}
+
+	startTime = std::chrono::system_clock::now();
+
+	// 视频播放主循环
+	while (true) {
+		handle_keyboard();
+		if (isPaused) {
+			pauseTime = std::chrono::duration<double>(std::chrono::system_clock::now() - startTime).count() - currentTime;
+		}
+		else {
+			currentTime = std::chrono::duration<double>(std::chrono::system_clock::now() - startTime).count() - pauseTime;
+		}
+		if (render_frame(lastFrame, lastIdx)) quit(0);
+	}
+}
+
+int main(int argc, char* argv[]) {
+	std::string videoName = "";
+	// 获取视频文件名称
+	if (argc < 2) {
+		std::cout << "请输入视频路径：";
+		getline(std::cin, videoName);
+	}
+	else {
+		videoName += argv[1];
+	}
 
 	if (videoName[0] == '\"') {
 		videoName.erase(videoName.end() - 1);
 		videoName.erase(videoName.begin());
 	}
 
-	// 打开视频文件
-	if (!video.open(videoName)) {
-		cerr << "ERROR: 无法打开视频文件" << endl;
-		exit(1);
+	if (init_resources(videoName)) {
+		system("pause");
+		return -1;
 	}
 
-	// 提取音频并保存为 WAV 文件
-	string command = ffmpegPath + " -i \"" + videoName + "\" -f wav -vn " + tmpAudioFile;
-	if (system(command.c_str()) != 0) { // 检查 ffmpeg 命令是否执行成功
-		cerr << "ERROR: 音频提取失败" << endl;
-		video.release();
-		exit(1);
-	}
-}
-
-
-// 播放音频
-void play_audio() {
-	// 加载音频文件到缓冲区
-	if (SDL_LoadWAV(tmpAudioFile.c_str(), &wavSpec, &wavBuffer, &wavLength) == NULL) {
-		cerr << "ERROR: 无法加载音频文件: " << SDL_GetError() << endl;
-		SDL_Quit();
-		exit(1);
-	}
-
-	// 打开音频设备
-	deviceId = SDL_OpenAudioDevice(NULL, 0, &wavSpec, NULL, 0);
-	if (deviceId == 0) {
-		cerr << "ERROR: 无法打开音频设备: " << SDL_GetError() << endl;
-		SDL_FreeWAV(wavBuffer);
-		SDL_Quit();
-		exit(1);
-	}
-
-	// 开始播放音频
-	SDL_QueueAudio(deviceId, wavBuffer, wavLength);
-	SDL_PauseAudioDevice(deviceId, 0);
-}
-
-
-// 停止音频并清理资源
-void stop_audio() {
-	SDL_CloseAudioDevice(deviceId);
-	SDL_FreeWAV(wavBuffer);
-	remove(tmpAudioFile.c_str());
-}
-
-
-// 暂停/播放事件处理
-void pause_audio() {
-	isPaused = !isPaused;
-	SDL_PauseAudioDevice(deviceId, isPaused ? 1 : 0);
-}
-
-
-// 计算字符画尺寸
-void get_size(int& width, int& height) {
-	CONSOLE_SCREEN_BUFFER_INFO csbi;
-	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
-	width = csbi.srWindow.Right - csbi.srWindow.Left << 1;
-	height = csbi.srWindow.Bottom - csbi.srWindow.Top << 1;
-
-	double aspectRatio = (double)csbi.dwSize.X / width / csbi.dwSize.Y * height;
-
-	// 调整宽高比
-	if ((double)video.get(CAP_PROP_FRAME_WIDTH) / width > aspectRatio * video.get(CAP_PROP_FRAME_HEIGHT) / height) {
-		height = aspectRatio * width / video.get(CAP_PROP_FRAME_WIDTH) * video.get(CAP_PROP_FRAME_HEIGHT);
-		height = height >> 1 << 1; // 确保高度为偶数
-	}
-	else {
-		width = (double)height / video.get(CAP_PROP_FRAME_HEIGHT) * video.get(CAP_PROP_FRAME_WIDTH) / aspectRatio;
-	}
-}
-
-
-// 清理资源并退出
-void stop_and_exit() {
-	stop_audio();
-	puts("\33[0m\n\33[?25h\n\33c");
-	video.release();
-	SDL_Quit();
-	exit(0);
-}
-
-
-// 键盘事件处理
-void handle_keyboard() {
-	if (_kbhit()) {
-		char ch = _getch();
-		switch (ch) {
-		case SPACE:
-			pause_audio();
-			break;
-		case ESC:
-			stop_and_exit();
-		default:
-			break;
-		}
-	}
-}
-
-
-void play() {
-	int width, height, d, lost = 0, totalFrames = video.get(CAP_PROP_FRAME_COUNT);
-	Mat img;
-	Vec3b color, bgColor = { 0, 0, 0 }, txtColor = { 0, 0, 0 };
-	auto startTime = high_resolution_clock::now();
-	string output;
-
-	// 判断颜色差异是否超过阈值，超过则返回 true
-	auto f = [&d](Vec3b x, Vec3b y) -> bool {
-		return max({ abs(x[0] - y[0]), abs(x[1] - y[1]), abs(x[2] - y[2]) }) > d;
-		};
-
-	// 获取当前播放时间并将其转换为视频帧数
-	auto getTime = [&]() -> double {
-		currentTime = (duration<double>(high_resolution_clock::now() - startTime).count() - pausedTime);
-		return currentTime * video.get(CAP_PROP_FPS);
-		};
-
-	puts("\33c\n\33[?25l\n\33[30;40m"); // 清屏并隐藏光标
-	play_audio();
-
-	// 视频播放主循环
-	for (int idx = 0; idx < totalFrames; ++idx) {
-		handle_keyboard();
-		while (isPaused) {
-			SDL_Delay(50);
-			handle_keyboard();
-			pausedTime = duration<double>(high_resolution_clock::now() - startTime).count() - currentTime;
-		}
-
-		Mat tmp;
-		double current = getTime();
-		if (!video.read(tmp)) break;
-
-		if (current > idx) { // 跳过丢失的帧
-			++lost;
-			continue;
-		}
-		while (current < idx) current = getTime(); // 等待同步
-
-		get_size(width, height);
-
-		Mat frame;
-		bool firstFrame = 1, colorChanged = 1;
-		d = pow(lost, exponential); // 根据丢帧数调整颜色差异阈值
-		lost = 0;
-		output.clear();
-		resize(tmp, frame, { width, height }); // 调整图像尺寸
-
-		// 如果尺寸变化则清屏
-		if (img.cols != width || img.rows != height) {
-			img = Mat(width, height, CV_8UC1, Scalar(0, 0, 0));
-			puts("\33c");
-		}
-
-		for (int i = 0; i < height; i += 2) {
-			for (int j = 0; j < width; ++j) {
-				if (!f(frame.at<Vec3b>(i, j), img.at<Vec3b>(i, j)) && !f(frame.at<Vec3b>(i + 1, j), img.at<Vec3b>(i + 1, j))) {
-					firstFrame = 1;
-					continue; // 如果屏幕上颜色和图像颜色相同则跳过输出
-				}
-				if (firstFrame) {
-					firstFrame = colorChanged = 0;
-					output += "\33[" + to_string(i / 2 + 1) + ';' + to_string(j + 1) + 'H'; // 移动光标
-				}
-				color = frame.at<Vec3b>(i, j);
-				if (f(color, bgColor)) { // 改变背景颜色
-					output += "\33[48;2;" + to_string(color[2]) + ';' + to_string(color[1]) + ';' + to_string(color[0]) + 'm';
-					bgColor = color;
-				}
-				color = frame.at<Vec3b>(i + 1, j);
-				if (f(color, txtColor)) { // 改变文本颜色
-					output += "\33[38;2;" + to_string(color[2]) + ';' + to_string(color[1]) + ';' + to_string(color[0]) + 'm';
-					txtColor = color;
-				}
-				output += "▄"; // 输出字符
-			}
-			if (!firstFrame && i + 2 < height) output += '\n';
-		}
-		if (!colorChanged) fputs(output.c_str(), stdout); // 打印输出
-		img = frame; // 更新图像
-	}
-
-	return;
-}
-
-
-int main(int argc, char* argv[]) {
-	get_cfg();
-	init();
-	get_file(argc, argv);
 	play();
-	stop_and_exit();
+
+	return 0;
 }
+
